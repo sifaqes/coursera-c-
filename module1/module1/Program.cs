583730966c63bb8e7d0576ddc1df3bf2dfa35f59
Sifaqes Zerrouki <64240372+sifaqes@users.noreply.github.com>
2023-03-20 15:29:24 +0100

    PROPRIETER Y CONSTRUCTORS

--- module1/module1/Program.cs
+++ module1/module1/Program.cs
@@ -10,6 +10,59 @@ namespace module1
     {
         static void Main(string[] args)
         {
+            //Person p1 = new Person("siphax", "zerrouki",27) ;
+            //Console.WriteLine($"The result of your class {p1.sayAll}");
+
+            Person p2 = new Person();
+            Console.WriteLine(p2.sayAll);
+            Console.ReadLine();
         }
+    }
+
+
+    public class Person
+    {
+        private string Fname;
+        private string Lname;
+        private int Age;
+
+        //proprietis
+
+        //Constructor
+        public Person(string Fname, string Lname, int Age) 
+        {
+            this.Lname = Lname;
+            this.Fname = Fname;
+            this.Age = Age;
+        }
+
+        public Person():this("Empty","Empty",0)
+        {
+        }
+
+        public string sayAll
+        {
+            get
+            {
+                return $"P2: {Fname}, {Lname}, {Age}";
+            }
+            set 
+            {
+                Fname= value;
+                Lname = value;
+                if (int.Parse(value) < 0)
+                {
+                    Console.WriteLine($"The age most be less a 0");
+                }
+                else
+                {
+                    Age = int.Parse(value);
+                }
+                
+
+            }
+        }
+
+       
     }
 }
