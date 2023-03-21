9061c4be98a7e761a25aa355f4a1b390e79e53b6
Sifaqes Zerrouki <64240372+sifaqes@users.noreply.github.com>
2023-03-21 15:10:27 +0100

    module 2

---
+++ module1/module1/module2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace module1
+{
+    internal class module2
+    {
+        static void Main(string[] args) 
+        {
+            int x = 10;
+            Console.WriteLine($"x={x}");
+            Console.ReadLine();
+        }
+    }
+}
